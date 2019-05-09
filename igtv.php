ff5135b14cc94e97df1140b00e0ba680e9e8a149
DanielCandido <daniel@musardos.com.br>
2019-05-09 13:49:23 -0300

    Arrumando layout e listando video para download

--- igtv.php
+++ igtv.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script type="text/javascript" src="js/FileSaver.js"></script>
 	<script type="text/javascript" src="js/download.js"></script>
@@ -8,15 +9,15 @@
 	<title></title>
 </head>
 <body>
-
 	<div style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;"><div class="Nnq7C weEfm"><a class="_bz0w" href="https://www.instagram.com/tv/BwupFYKFpkD/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/b8b01759371fa831aca0f658f47e9a02/5CD4AF06/t51.2885-15/e35/56749581_2121070764678873_7304305441585067375_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">3:04</div></div><div class="pu1E0"><div class="_2XLe_">Como Você pode organizar seu cronograma de conteúdo</div></div></div></div></div></a><a class="_bz0w" href="https://www.instagram.com/tv/BvzvAZcFPKK/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/9bf6fab2c45b28c06aa84ec201e5b997/5CD422CF/t51.2885-15/e35/54423281_547434632331450_4495322041529829947_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">3:54</div></div><div class="pu1E0"><div class="_2XLe_">Uso de Bots para Aumentar Seguidores</div></div></div></div></div></a><a class="_bz0w" href="https://www.instagram.com/tv/BvPicTtFjUR/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/b7fd213cdc1ad71579966f1a82f2fa18/5CD43205/t51.2885-15/e35/53618415_361746248014598_1563873156990489363_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">3:03</div></div><div class="pu1E0"><div class="_2XLe_">Hoax da Momo ressurge na internet...</div></div></div></div></div></a></div><div class="Nnq7C weEfm"><a class="_bz0w" href="https://www.instagram.com/tv/BvCoCotFqs_/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/db7ba2c258956a58f4bb614ca533f6dd/5CD4220B/t51.2885-15/e35/54277713_807365256316520_267740792883383776_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">2:07</div></div><div class="pu1E0"><div class="_2XLe_">Games influenciam a vida dos adolescentes</div></div></div></div></div></a><a class="_bz0w" href="https://www.instagram.com/tv/BtOYbW1H8k0/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/89567063ee54dd5401bdbb428b20d902/5CD43305/t51.2885-15/e35/50041802_370795340140741_1949149026928570382_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">3:47</div></div><div class="pu1E0"><div class="_2XLe_">Influenciadores esse vídeo é para vocês!!!</div></div></div></div></div></a><a class="_bz0w" href="https://www.instagram.com/tv/Bs_nPWml08j/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/2e8312a98147c0f5b2ccbeecaa6c0ec0/5CD4AFB7/t51.2885-15/e35/50046063_369111523911107_2276446782189972943_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">2:13</div></div><div class="pu1E0"><div class="_2XLe_">Brincadeira e Humor para uma marca, vale adotar?</div></div></div></div></div></a></div><div class="Nnq7C weEfm"><a class="_bz0w" href="https://www.instagram.com/tv/BsyhbdmFBIy/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/232980247b3ea5c7e05c2ac364735dd0/5CD39E28/t51.2885-15/e35/49443941_542923219526250_6759605421780412294_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">3:50</div></div><div class="pu1E0"><div class="_2XLe_">Engajamento no Instagram</div></div></div></div></div></a><a class="_bz0w" href="https://www.instagram.com/tv/BsycdspFL3J/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/a91f2f1a58202d671ff213feb8207afe/5CD4A169/t51.2885-15/e35/49756242_384011079022710_8679350917744800189_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">2:18</div></div><div class="pu1E0"><div class="_2XLe_">Como aumentar o número de seguidores no Instagram</div></div></div></div></div></a><a class="_bz0w" href="https://www.instagram.com/tv/Bn4mDShhZkf/"><div role="button" tabindex="0" class="A-NpN"><div class="lVhHa RNL1l" style="background-image: url(&quot;https://instagram.fbfh2-1.fna.fbcdn.net/vp/9a485cbfa8caecd7ff79c4196f5676f8/5CD4183D/t51.2885-15/e35/40619304_277975836371337_8783423561451707724_n.jpg?_nc_ht=instagram.fbfh2-1.fna.fbcdn.net&quot;); display: block; padding-top: 155.399%; width: 100%;"></div><div class="qn-0x"><div class="_5cOAs"><div class="Rsx-c"><div class="zncDM">4:35</div></div><div class="pu1E0"><div class="_2XLe_">Eleições na internet e as mobilizações</div></div></div></div></div></a></div></div>
-	<p id="download" style="background-color: pink; height: 500px;"></p>
+	
 
 </body>
 <script type="text/javascript">
 	var divs = document.getElementsByClassName('_bz0w');
 	//document.getElementsByClassName('RNL1l').style.display = "none";
 	var titulos = [];
+
 
 	var elems = document.getElementsByClassName("_bz0w");
 	for(var i =0; i < elems.length; i++){
@@ -36,7 +37,7 @@
 		names.push({href : allDivs[i].href, titulo : allDivs[i].innerText},);
 	}
 
-	console.log(names.length);
+	//console.log(names.length);
 
 	for (var i = 0; i < names.length; i++) {
 		this.titulos.push(names[i].titulo);
@@ -58,7 +59,9 @@
 		});
 	}
 </script>
+
 <script type="text/javascript">
+	var attr = [];
 	function arrumaHtml(){
 		var svg = document.getElementsByTagName('svg');
 		//console.log(svg);   
@@ -70,10 +73,18 @@
 		var body = document.getElementsByTagName('body');
 		for(let i = 0; i < page.length; i++){
 			page[i].style.height = '100%';
+			body[i].style.background = '#363636';
 			body[i].style.height = '100%';
 		}
 
-
+		var img = document.createElement('img');
+		img.src = "videos/logo.png";
+		img.style.textAling = "center";
+		img.style.width = "120px";
+		img.style.height = "120px";
+		img.style.borderRadius = "50%";
+		img.style.background = "#00BFFF	";
+		document.body.appendChild(img);
 	}
 
 
@@ -82,30 +93,69 @@
 		console.log(this.titulos);
 		
 		var met = document.querySelectorAll("meta[property='og:video']");
+		var titles = document.querySelectorAll("meta[property='og:title']");
 		//console.log(met);
-		var attr = [];
-		var urls = [];
+		
+		var title = [].slice.call(titles);
 		var html = [].slice.call(met);
 
 		//console.log(html);
-		this.titulos.reverse();
 		for(let i =0; i < html.length; i++){
-			attr.push({url : html[i].content, nome : this.titulos[i]},);
+			this.attr.push({url : html[i].content, nome : title[i].content+'.mp4'},);
 		}
 
-		for(let i = 0; i < attr.length; i++){
-			this.baixaVideo(attr[i].url, attr[i].nome);
+		for(let i = 0; i < this.attr.length; i++){
+			var button = document.createElement('button');
+		    button.setAttribute('type','button');
+		    button.style.background = "#483D8B";
+		    button.style.color = "white";
+		    button.style.borderRadius = "5px";
+		    button.style.padding = "8px";
+		    button.style.borderColor = "black";
+		    button.style.width = "100%"; 
+		    button.style.marginTop = "10px";
+		    button.appendChild(document.createTextNode(this.attr[i].nome));
+		    button.addEventListener('click', function baixaVideo(){
+		    	console.log('download iniciado');
+		    	window.location = "download.php?arquivo="+attr[i].url+"&nome="+attr[i].nome;
+		    });
+		    document.body.appendChild(button);
 		}
 
 
-		console.log(attr);
+		//console.log(this.attr);
+		this.baixaTodos();
 		
 	}
 
-	function baixaVideo(url, nome){
+	/*function baixaVideo(url, nome){
 		console.log("baixaVideo")
 	    window.location = "download.php?arquivo="+url+"&nome="+nome+".mp4";
 	}	  
+*/
+
+	function baixaTodos(){
+		var length = this.attr.length;
+		console.log(length);
+
+		var button = document.createElement('button');
+		    button.setAttribute('type','button')
+		    button.appendChild(document.createTextNode('Baixar Todos'));
+		    button.style.background = "yellow";
+		    button.style.height = "40px";
+		    button.style.marginTop = "20px";
+		    button.style.borderRadius = "5px";
+		    button.style.textAling = "center";
+		    button.style.borderColor = "black";
+		    button.addEventListener('click', function baixar(){
+
+		    	for(let i = 0; i < length; i++){
+			    	console.log('download iniciado');
+			    	window.location = "download.php?arquivo="+attr[i].url+"&nome="+attr[i].nome;
+		    	}
+		    });
+		    document.body.appendChild(button);
+	}
 
 		setTimeout(this.pegaUrl, 4000);
 		setTimeout(this.arrumaHtml, 3000);
